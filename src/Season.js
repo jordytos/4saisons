a22bf80ae5c54438c408df6c232bae786bc97939
jordytos <jordynabina@gmail.com>
2022-02-16 18:56:23 +0100

    Partie 1 finished | Parcel, Date-fns, AlgoCours, Css

---
+++ src/Season.js
@@ -0,0 +1,26 @@
+import { formatDistanceToNowStrict } from "date-fns";
+
+import "./Season.css";
+import data from "./data.json";
+
+function Season({ name }) {
+  const season = data.seasons[name];
+  const currentYear = new Date().getFullYear();
+  const distance = formatDistanceToNowStrict(
+    new Date(
+      currentYear + (name === "winter" ? -1 : 0),
+      season.beginAt.month - 1,
+      season.beginAt.day
+    ),
+    { unit: "day", addSuffix: true }
+  );
+  return (
+    <div className="Season-root">
+      <img src={season.icon} aria-label={name} className="Season-icon"></img>
+      <h1 className="Season-title">{season.name}</h1>
+      <p className="Season-content">{distance}</p>
+    </div>
+  );
+}
+
+export default Season;
