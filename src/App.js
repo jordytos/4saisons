ffd7abfc64545aa10026df289841379b0e62d39a
jordytos <jordynabina@gmail.com>
2022-02-17 01:09:47 +0100

    Ajout Formater & pre-commit check

--- src/App.js
+++ src/App.js
@@ -6,59 +6,57 @@ import Season from "./Season";
 import Modal from "./Modal";
 import data from "./data.json";
 
-
 const NextSeason = lazy(() => import("./NextSeason"));
 
 const current = Object.entries(data.seasons).filter(([name, season]) => {
-    const currentYear = new Date().getFullYear();
-    if (name === "winter") {
-      return (
-        isWithinInterval(new Date(), {
-          start: new Date(
-            currentYear - 1,
-            season.beginAt.month - 1,
-            season.beginAt.day
-          ),
-          end: new Date(currentYear, season.endAt.month - 1, season.endAt.day),
-        }) ||
-        isWithinInterval(new Date(), {
-          start: new Date(
-            currentYear,
-            season.beginAt.month - 1,
-            season.beginAt.day
-          ),
-          end: new Date(
-            currentYear + 1,
-            season.endAt.month - 1,
-            season.endAt.day
-          ),
-        })
-      );
-    }
-    return isWithinInterval(new Date(), {
-      start: new Date(currentYear, season.beginAt.month - 1, season.beginAt.day),
-      end: new Date(currentYear, season.endAt.month - 1, season.endAt.day),
-    });
-  })[0];
-
-
-function App() {
+  const currentYear = new Date().getFullYear();
+  if (name === "winter") {
     return (
-      <div className="App-root">
-        <main className="App-main">
-          <div className="App-content">
-            <Season name={current[0]} />
-          </div>
-          <div className="App-actions">
-            <Modal label="Et après ?">
-              <Suspense fallback={<div>Loading...</div>}>
-                <NextSeason name={current[1].next} />
-              </Suspense>
-            </Modal>
-          </div>
-        </main>
-      </div>
+      isWithinInterval(new Date(), {
+        start: new Date(
+          currentYear - 1,
+          season.beginAt.month - 1,
+          season.beginAt.day
+        ),
+        end: new Date(currentYear, season.endAt.month - 1, season.endAt.day),
+      }) ||
+      isWithinInterval(new Date(), {
+        start: new Date(
+          currentYear,
+          season.beginAt.month - 1,
+          season.beginAt.day
+        ),
+        end: new Date(
+          currentYear + 1,
+          season.endAt.month - 1,
+          season.endAt.day
+        ),
+      })
     );
   }
+  return isWithinInterval(new Date(), {
+    start: new Date(currentYear, season.beginAt.month - 1, season.beginAt.day),
+    end: new Date(currentYear, season.endAt.month - 1, season.endAt.day),
+  });
+})[0];
+
+function App() {
+  return (
+    <div className="App-root">
+      <main className="App-main">
+        <div className="App-content">
+          <Season name={current[0]} />
+        </div>
+        <div className="App-actions">
+          <Modal label="Et après ?">
+            <Suspense fallback={<div>Loading...</div>}>
+              <NextSeason name={current[1].next} />
+            </Suspense>
+          </Modal>
+        </div>
+      </main>
+    </div>
+  );
+}
 
 export default App;
